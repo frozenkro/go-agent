ff8484cf5122579d6641a3915e6e9aec24f20b9d
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-08-25 08:40:48 -0500

    Initial commit, anthropic poc

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/frozenkro/go-agent
+
+go 1.24.6
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+const ANTHROPIC_MESSAGES_URL = "https://api.anthropic.com/v1/messages"
+const TEST_REQUEST = `
+{
+    "model": "claude-sonnet-4-20250514",
+    "max_tokens": 1024,
+    "messages": [
+        {"role": "user", "content": "Hello, world"}
+    ]
+}
+`
+
+const EXEC_TOOL_SCHEMA = `
+{
+	"name": "execute_command",
+	"description": "Run any common bash command, such as 'ls', 'cd', 'cat', 'sed', etc.",
+	"input_schema": {
+		"type": "object",
+		"properties": {
+			"command": {
+					"type": "string",
+					"description": "Arbitrary bash command"
+			}
+		},
+		"required": ["command"]
+	}
+}
+`
+
+func main() {
+	ctx := context.Background()
+	godotenv.Load()
+
+	apiKey := os.Getenv("GA_ANTHROPIC_API_KEY")
+	bodyReader := bytes.NewReader([]byte(TEST_REQUEST))
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, ANTHROPIC_MESSAGES_URL, bodyReader)
+	if err != nil {
+		log.Fatalf("%v", err.Error())
+	}
+
+	req.Header.Add("x-api-key", apiKey)
+	req.Header.Add("anthropic-version", "2023-06-01")
+	req.Header.Add("content-type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("%v", err.Error())
+	}
+	defer res.Body.Close()
+
+	content, err := io.ReadAll(res.Body)
+
+	log.Printf("Response:\n%v", string(content))
+}
