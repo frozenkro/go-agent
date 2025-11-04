096fb961b11ca1c1d76a1ba002eb0524147c7e62
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-11-03 19:43:54 -0600

    Add ollama models

--- models/ollama/request.go
+++ models/ollama/request.go
@@ -1,3 +1,58 @@
+// derived from docs: https://docs.ollama.com/api/chat
 package ollama
 
-type OllamaRequest struct{}
+type OllamaRequest struct {
+	Model     string              `json:"model"`
+	Messages  Message             `json:"messages"`
+	Tools     []Tool              `json:"tools"`
+	Format    OllamaRequestFormat `json:"format"` // this can also be a json schema, which is not elaborated on in the docs
+	Options   *Options            `json:"options,omitempty"`
+	Stream    bool                `json:"stream"`
+	Think     bool                `json:"think"`
+	KeepAlive string              `json:"keep_alive"`
+}
+
+type Role string
+
+const (
+	SYSTEM_ROLE    Role = "system"
+	USER_ROLE      Role = "user"
+	ASSISTANT_ROLE Role = "assistant"
+	TOOL_ROLE      Role = "tool"
+)
+
+type Message struct {
+	Role      Role   `json:"role"`
+	Content   string `json:"content"`
+	Thinking  bool   `json:"thinking"`
+	ToolCalls []any  `json:"tool_calls,omitempty"`
+}
+
+type Tool struct {
+	Type OllamaToolType // always "function"
+}
+
+type OllamaToolType string
+
+const Function OllamaToolType = "function"
+
+type OllamaToolFunction struct {
+	Name        string `json:"name"`
+	Parameters  any    `json:"parameters"`
+	Description string `json:"description"`
+}
+
+type OllamaRequestFormat string
+
+const JSON_FORMAT OllamaRequestFormat = "json"
+
+type Options struct {
+	Seed        int      `json:"seed"`
+	Temperature int      `json:"temperature"`
+	TopK        int      `json:"top_k"`
+	TopP        int      `json:"top_p"`
+	MinP        int      `json:"min_p"`
+	Stop        []string `json:"stop"`
+	NumCtx      int      `json:"num_ctx"`
+	NumPredict  int      `json:"num_predict"`
+}
--- models/ollama/response.go
+++ models/ollama/response.go
@@ -1,3 +1,15 @@
 package ollama
 
-type OllamaResponse struct{}
+type OllamaResponse struct {
+	Model              string  `json:"model"`
+	CreatedAt          string  `json:"created_at"`
+	Message            Message `json:"message"`
+	Done               bool    `json:"done"`
+	DoneReason         bool    `json:"done_reason"`
+	TotalDuration      int     `json:"total_duration"`
+	LoadDuration       int     `json:"load_duration"`
+	PromptEvalCount    int     `json:"prompt_eval_count"`
+	PromptEvalDuration int     `json:"prompt_eval_duration"`
+	EvalCount          int     `json:"eval_count"`
+	EvalDuration       int     `json:"eval_duration"`
+}
